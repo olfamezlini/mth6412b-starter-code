b334903c56406a904cf93afe2aba58f03733e426
olfamezlini <olfa.mezlini@etudiant-enit.utm.tn>
2024-09-23 19:59:00 -0400

    Cloture phase 1

--- src/phase1/pluto_phase1_template.jl
+++ src/phase1/pluto_phase1_template.jl
@@ -13,21 +13,26 @@ using InteractiveUtils
 # ╔═╡ 7ea81498-700d-4402-bea4-37b5203d088f
 using Logging
 
+# ╔═╡ 31fd4c5e-2b7b-4752-84c5-8fa1fae3d112
+md"""
+#### Travail réalisé par : Olfa Mezlini et Giorgi Gamkrelidze
+"""
+
 # ╔═╡ 5b0505f0-ab35-4ebc-9458-1914a54c7bfa
 md"""
-### Mini rapport: Phase 1 du projet
+### Phase 1 du projet
 """
 
 # ╔═╡ 56c69abe-5f9d-433d-87ee-ad4d14d7e4ac
 md"""
 ##### 1. Récupérer le code de la phase 1 sur le site web du cours. Vous y trouverez une structure de données Node pour les noeuds d’un graphe et Graph pour le graphe proprement dit. On fournit également un jeu de fonctions qui lisent les fichiers au format de TSPLib. On se limite ici aux problèmes symétriques dont les poids sont donnés au format EXPLICIT . """
 
+# ╔═╡ 2ca058c6-5f4b-47d1-9282-a8803eaf19c5
+md""" On a créé un fork pour le projet fournit sur GitHub "MTH6412b-starter-code : Une fois que nous avons forké le projet, nous pouvons apporter toutes les modifications que nous souhaitons à cette copie. Cela nous permet de travailler sur les nouvelles fonctionnalités, de répondre aux questions et faire le travail demandé sans interférer avec le dépôt original."""
+
 # ╔═╡ c49caeea-0bdd-4cd3-8e50-c739befecd98
 md"""[https://github.com/olfamezlini/mth6412b-starter-code/tree/Phase-1/src/phase1](https://github.com/olfamezlini/mth6412b-starter-code/tree/Phase-1/src/phase1)"""
 
-
-# ╔═╡ 2ca058c6-5f4b-47d1-9282-a8803eaf19c5
-md""" On a créé un fork pour le projet fournit sur GitHub "MTH6412b-starter-code : Une fois que nous avons forké le projet, nous pouvons apporter toutes les modifications que nous souhaitons à cette copie. Cela vous permet de travailler sur les nouvelles fonctionnalités, de répondre aux questions et faire le travail demandé sans interférer avec le dépôt original."""
 
 # ╔═╡ d16af9e4-e9fd-49df-97e4-b6af7193d63f
 md"""
@@ -35,7 +40,7 @@ md"""
 """
 
 # ╔═╡ 11e677b5-6ac6-45f2-a5ad-4ad36fd2776c
-md""" Nous avons définit la structure Edge de maniere semblale a celle de Node. La structure est décrite alors par un nom de type String, un poids d'un type général (DATA) et de deux noeuds de type Node. """
+md""" Nous avons définit la structure Edge de manière semblale a celle de Node. La structure est décrite alors par un nom de type String, un poids d'un type général (DATA) et de deux noeuds de type Node. """
 
 # ╔═╡ 789d0351-e2db-4250-8694-7003fbe1c17b
 """Type abstrait dont d'autres types d'arrêtes dériveront."""
@@ -83,7 +88,7 @@ md"""
 ###### Méthode d'affichage pour un type edge"""
 
 # ╔═╡ b2dde163-2e25-4478-aeab-ba34df4e0f25
-md""" Pour afficher une arête, on a créé la fonction show qui prend comme paramètre une arête et affiche les informations lui associer : """
+md""" Pour afficher une arête, on a créé la fonction "show" qui prend comme paramètre une arête et affiche les informations associées : """
 
 # ╔═╡ 623cc0bd-c29f-47c4-aab6-fa948cbb331e
 md"""
@@ -124,7 +129,7 @@ md"""
 """
 
 # ╔═╡ 60e6dd05-1c53-441c-a92a-bfc5505f7db1
-md""" Nous avons modifié le fichier Graph.jl, la fonction add_edge a été ajouté. """
+md""" Nous avons modifié le fichier "graph.jl", la fonction add_edge a été ajouté. """
 
 # ╔═╡ cc928741-3463-4b58-b552-af710f92ae30
 md"""
@@ -142,9 +147,23 @@ md"""
 
 """
 
-# ╔═╡ 737fea6b-ee3f-42d6-964d-de2bcbe9c242
-md""" Afin que la fonction show graph prenne en considération les arêtes et les affiche, la modification suivante était faite : 
+# ╔═╡ a8a131a0-461a-46fb-9116-b7f6f5c254ad
+md"""
+###### Exemple"""
+
+# ╔═╡ c290313e-06cd-4b07-84f5-8ec5eeb9caf9
+md""" Voici un exemple de création d'un graphe composé de deux noeuds et une arête. """
+
+# ╔═╡ e4c9cea3-7a60-4b77-b52f-1be16f63fa2c
+md"""
+```julia
+G = Graph("Ick", [node1, node2], [edge])
+show(G)
+```
 """
+
+# ╔═╡ f4494162-20d0-476e-8401-8743bb4e6a76
+md""" Les modifications suivantes étaient faites sur la fonction "show" afin qu'on peut afficher les arêtes aussi. """
 
 # ╔═╡ 61c286e5-cba5-43f0-ba50-6a4e219ba42a
 md"""
@@ -161,11 +180,20 @@ end
 ```
 """
 
+# ╔═╡ 8eaa7f80-f936-4bfa-a36b-53a8c7260a0c
+md""" Le résultat de code ci-dessus est le suivant : """
+
+# ╔═╡ a01e331c-013b-48fb-bce1-ef5f3d87452f
+md""" Graph Ick has 2 nodes and 1 edges. """
+
 # ╔═╡ 281a9ab0-29c5-4491-8bb2-b1bed1b63eff
 md"""
 ##### 5.  Étendre la fonction read_edges() de read_stsp.jl afin de lire les poids des arêtes (ils sont actuellement ignorés).
 
 """
+
+# ╔═╡ 40f5ec01-d6b7-4a63-ae95-600b23cba581
+md""" La fonction ``` read_edges``` a été modifiée de façon à prendre en compte les poids. """
 
 # ╔═╡ bbdffecf-e537-4562-ba0e-b3e006965992
 md"""
@@ -173,8 +201,6 @@ md"""
 function read_edges(header::Dict{String}{String}, filename::String)
 
   edges = []
-  # Définition d'une liste pour stocker les poids
-  edge_weights = []
   edge_weight_format = header["EDGE_WEIGHT_FORMAT"]
   known_edge_weight_formats = ["FULL_MATRIX", "UPPER_ROW", "LOWER_ROW",
   "UPPER_DIAG_ROW", "LOWER_DIAG_ROW", "UPPER_COL", "LOWER_COL",
@@ -182,8 +208,115 @@ function read_edges(header::Dict{String}{String}, filename::String)
 
   if !(edge_weight_format in known_edge_weight_formats)
     @warn "unknown edge weight format" edge_weight_format
-    return edges, edge_weights
+    return edges
   end
+
+  file = open(filename, "r")
+  dim = parse(Int, header["DIMENSION"])
+  edge_weight_section = false
+  k = 0
+  n_edges = 0
+  i = 0
+  n_to_read = n_nodes_to_read(edge_weight_format, k, dim)
+  flag = false
+
+  for line in eachline(file)
+    line = strip(line)
+    if !flag
+      if occursin(r"^EDGE_WEIGHT_SECTION", line)
+        edge_weight_section = true
+        continue
+      end
+
+      if edge_weight_section
+        data = split(line)
+        n_data = length(data)
+        start = 0
+        while n_data > 0
+          n_on_this_line = min(n_to_read, n_data)
+
+          for j = start : start + n_on_this_line - 1
+            n_edges = n_edges + 1
+            if edge_weight_format in ["UPPER_ROW", "LOWER_COL"]
+              edge = (k+1, i+k+2)
+            elseif edge_weight_format in ["UPPER_DIAG_ROW", "LOWER_DIAG_COL"]
+              edge = (k+1, i+k+1)
+            elseif edge_weight_format in ["UPPER_COL", "LOWER_ROW"]
+              edge = (i+k+2, k+1)
+            elseif edge_weight_format in ["UPPER_DIAG_COL", "LOWER_DIAG_ROW"]
+              edge = (i+1, k+1)
+            elseif edge_weight_format == "FULL_MATRIX"
+              edge = (k+1, i+1)
+            else
+              warn("Unknown format - function read_edges")
+            end
+            push!(edges, edge)
+            i += 1
+          end
+
+          n_to_read -= n_on_this_line
+          n_data -= n_on_this_line
+
+          if n_to_read <= 0
+            start += n_on_this_line
+            k += 1
+            i = 0
+            n_to_read = n_nodes_to_read(edge_weight_format, k, dim)
+          end
+
+          if k >= dim
+            n_data = 0
+            flag = true
+          end
+        end
+      end
+    end
+  end
+  close(file)
+  return edges
+end
+```
+"""
+
+# ╔═╡ f5bc3fb5-c45e-43b6-89ac-ff7d7edf2268
+md""" Ainsi que la fonction ``` read_stps ``` a également dû être modififié pour la récupération des poids pour permettre l'affichage. """
+
+# ╔═╡ 5778a4fe-3e08-4eee-bdc7-6e815a3b1672
+md"""
+```julia
+function read_stsp(filename::String)
+  Base.print("Reading of header : ")
+  header = read_header(filename)
+  println("✓")
+  dim = parse(Int, header["DIMENSION"])
+  edge_weight_format = header["EDGE_WEIGHT_FORMAT"]
+
+  Base.print("Reading of nodes : ")
+  graph_nodes = read_nodes(header, filename)
+  println("✓")
+
+  Base.print("Reading of edges : ")
+  edges_brut = read_edges(header, filename)
+  graph_edges = []
+  for k = 1 : dim
+    edge_list = Int[]
+    push!(graph_edges, edge_list)
+  end
+
+  for edge in edges_brut
+    if edge_weight_format in ["UPPER_ROW", "LOWER_COL", "UPPER_DIAG_ROW", "LOWER_DIAG_COL"]
+      push!(graph_edges[edge[1]], edge[2])
+    else
+      push!(graph_edges[edge[2]], edge[1])
+    end
+  end
+
+  for k = 1 : dim
+    graph_edges[k] = sort(graph_edges[k])
+  end
+  println("✓")
+  return graph_nodes, graph_edges
+end
 ```
 """
 
@@ -194,7 +327,7 @@ md"""
 """
 
 # ╔═╡ ecca1940-1745-4b48-8183-ce1f0f7b18c2
-md""" Afin de répondre à cette question, nous avons créé un fichier nommé Main.jl, ou on a fait la Lecture de l'instance de TSP symétrique (bayg29.tsp) et on a construit l'objet de type graph correspondant. Vous pouvez voir ça sur la branche phase 1 en appuyant sur le lien ci-dessous. """
+md""" Afin de répondre à cette question, nous avons créé un fichier nommé main.jl, où on a fait la Lecture de l'instance de TSP symétrique (bayg29.tsp) et on a construit l'objet de type graph correspondant. Vous pouvez voir ça sur la branche phase 1 en appuyant sur le lien ci-dessous. """
 
 # ╔═╡ 60ae8928-896e-4ab1-81b3-5284ea72b686
 md"""[https://github.com/olfamezlini/mth6412b-starter-code/blob/Phase-1/src/phase1/main.jl](https://github.com/olfamezlini/mth6412b-starter-code/blob/Phase-1/src/phase1/main.jl)"""
@@ -231,15 +364,16 @@ uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
 """
 
 # ╔═╡ Cell order:
-# ╠═d60f02ce-0a83-4edb-9747-b3de9ba1ae2b
-# ╠═5dfaa4bb-f544-43df-a390-a9e326784fed
-# ╠═7ea81498-700d-4402-bea4-37b5203d088f
+# ╟─d60f02ce-0a83-4edb-9747-b3de9ba1ae2b
+# ╟─5dfaa4bb-f544-43df-a390-a9e326784fed
+# ╟─7ea81498-700d-4402-bea4-37b5203d088f
+# ╟─31fd4c5e-2b7b-4752-84c5-8fa1fae3d112
 # ╟─5b0505f0-ab35-4ebc-9458-1914a54c7bfa
 # ╟─56c69abe-5f9d-433d-87ee-ad4d14d7e4ac
-# ╟─c49caeea-0bdd-4cd3-8e50-c739befecd98
 # ╟─2ca058c6-5f4b-47d1-9282-a8803eaf19c5
+# ╟─c49caeea-0bdd-4cd3-8e50-c739befecd98
 # ╟─d16af9e4-e9fd-49df-97e4-b6af7193d63f
-# ╠═11e677b5-6ac6-45f2-a5ad-4ad36fd2776c
+# ╟─11e677b5-6ac6-45f2-a5ad-4ad36fd2776c
 # ╟─789d0351-e2db-4250-8694-7003fbe1c17b
 # ╟─905d4a8a-9d15-45b3-8e3a-ffb33cd835b3
 # ╟─d670b6ea-a115-4a5a-bf9e-0179b05fb447
@@ -256,16 +390,24 @@ uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
 # ╟─60e6dd05-1c53-441c-a92a-bfc5505f7db1
 # ╟─cc928741-3463-4b58-b552-af710f92ae30
 # ╟─30d35ba2-4789-4f93-be23-15c034ae0146
-# ╟─737fea6b-ee3f-42d6-964d-de2bcbe9c242
+# ╟─a8a131a0-461a-46fb-9116-b7f6f5c254ad
+# ╟─c290313e-06cd-4b07-84f5-8ec5eeb9caf9
+# ╟─e4c9cea3-7a60-4b77-b52f-1be16f63fa2c
+# ╟─f4494162-20d0-476e-8401-8743bb4e6a76
 # ╟─61c286e5-cba5-43f0-ba50-6a4e219ba42a
-# ╠═281a9ab0-29c5-4491-8bb2-b1bed1b63eff
+# ╟─8eaa7f80-f936-4bfa-a36b-53a8c7260a0c
+# ╟─a01e331c-013b-48fb-bce1-ef5f3d87452f
+# ╟─281a9ab0-29c5-4491-8bb2-b1bed1b63eff
+# ╟─40f5ec01-d6b7-4a63-ae95-600b23cba581
 # ╟─bbdffecf-e537-4562-ba0e-b3e006965992
+# ╟─f5bc3fb5-c45e-43b6-89ac-ff7d7edf2268
+# ╟─5778a4fe-3e08-4eee-bdc7-6e815a3b1672
 # ╟─621fe82a-ac2a-4b15-bdca-74925a115963
 # ╟─ecca1940-1745-4b48-8183-ce1f0f7b18c2
 # ╟─60ae8928-896e-4ab1-81b3-5284ea72b686
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
 
-
 import Pluto
 Pluto.run()
+
