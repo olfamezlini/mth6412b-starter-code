b9bc59b7346bc6388d50888c1c3f628f71e05c69
olfamezlini <olfa.mezlini@etudiant-enit.utm.tn>
2024-09-23 09:54:39 -0400

    Ajout de fichier graph.jl

--- src/phase1/graph.jl
+++ src/phase1/graph.jl
@@ -30,6 +30,68 @@ end
 
 """Renvoie le nom du graphe."""
 name(graph::AbstractGraph) = graph.name
+import Base.show
+
+"""Type abstrait dont d'autres types de graphes dériveront."""
+abstract type AbstractGraph{T} end
+
+"""Type representant un graphe comme un ensemble de noeuds.
+
+Exemple :
+
+    node1 = Node("Joe", 3.14)
+    node2 = Node("Steve", exp(1))
+    node3 = Node("Jill", 4.12)
+    G = Graph("Ick", [node1, node2, node3], [edge1, edge2, edge3])
+
+Attention, tous les noeuds doivent avoir des données de même type.
+"""
+mutable struct Graph{T} <: AbstractGraph{T}
+  name::String
+  nodes::Vector{Node{T}}
+  edges::Vector{Edge{T}}
+end
+
+"""Ajoute un noeud au graphe."""
+function add_node!(graph::Graph{T}, node::Node{T}) where T
+  push!(graph.nodes, node)
+  graph
+end
+
+"""Ajoute un arete au graphe."""
+function add_edge!(graph::Graph{T}, edge::Edge{T}) where T
+  push!(graph.edges, edge)
+  graph
+end
+
+# on présume que tous les graphes dérivant d'AbstractGraph
+# posséderont des champs 'name', 'nodes' et 'edges.
+
+"""Renvoie le nom du graphe."""
+name(graph::AbstractGraph) = graph.name
+
+"""Renvoie la liste des noeuds du graphe."""
+nodes(graph::AbstractGraph) = graph.nodes
+
+"""Renvoie le nombre de noeuds du graphe."""
+nb_nodes(graph::AbstractGraph) = length(graph.nodes)
+
+"""Renvoie la liste des aretes du graphe."""
+edges(graph::AbstractGraph) = graph.edges
+
+"""Renvoie le nombre de aretes du graphe."""
+nb_edges(graph::AbstractGraph) = length(graph.edges)
+
+"""Affiche un graphe"""
+function show(graph::Graph)
+  println("Graph ", name(graph), " has ", nb_nodes(graph), " nodes and ", nb_edges(graph), " edges.")
+  for node in nodes(graph)
+    show(node)
+  end
+  for edge in edges(graph)
+    show(edge)
+  end
+end
 
 """Renvoie la liste des noeuds du graphe."""
 nodes(graph::AbstractGraph) = graph.nodes
