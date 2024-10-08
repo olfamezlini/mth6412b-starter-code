e201ea6ef8e98811c10e29f0de7f53bb914d1b84
olfamezlini <olfa.mezlini@etudiant-enit.utm.tn>
2024-10-07 22:42:31 -0400

    Cloture phase 2

---
+++ src/phase 2/runtests.jl
@@ -0,0 +1,12 @@
+# Importation des fichiers nécessaires
+
+include("../src/phase1/main.jl")
+
+using Test
+
+@testset "algortihme_kruskal" begin
+  graph_nodes, graph_edges, edge_weights_dict = read_stsp("../../instances/stsp/exemple_phase_2.tsp");
+  arbre_minimal, poids_minimal = Algortihme_Kruskal(graph_edges, edge_weights_dict);
+  @test poids_minimal==37
+  @test typeof(arbre_minimal)==Graph{Int64, Int64}
+end
